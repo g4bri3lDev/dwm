671077105ed26b987c5abf647923f66b4c7f70db
gabriel <g.lackermeier@gmx.de>
2020-06-24 20:03:28 +0200

    added floatrules patch + onboard rules

--- config.def.h
+++ config.def.h
@@ -32,9 +32,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1,        50,50,500,500,        5 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,        50,50,500,500,        5 },
+	{ "Onboard",  NULL,	  NULL,	      0,	    1,		 -1,	    805,950,310,130,	  1 },
 };
 
 /* layout(s) */
